f396c80c8b8459424ce4a7b08a50cbceeea4962a
jack-ie <jackie919@outlook.com>
2022-01-16 15:32:04 -0600

    issues

--- public/js/login.js
+++ public/js/login.js
@@ -2,12 +2,13 @@ const loginFormHandler = async (event) => {
     event.preventDefault();
   
     // Collect values from the login form
+   
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
   
     if (email && password) {
       // POST request to the API endpoint
-      const response = await fetch('../controllers/api/userRoutes', {
+      const response = await fetch('/api/users/login', {
         method: 'POST',
         body: JSON.stringify({ email, password }),
         headers: { 'Content-Type': 'application/json' },
@@ -15,7 +16,7 @@ const loginFormHandler = async (event) => {
   
       if (response.ok) {
         // If successful, redirect the browser to the profile page
-        document.location.replace('/profile');
+        document.location.replace('/dashboard');
       } else {
         alert(response.statusText);
       }
@@ -24,20 +25,21 @@ const loginFormHandler = async (event) => {
   
   const signupFormHandler = async (event) => {
     event.preventDefault();
-  
-    const name = document.querySelector('#name-signup').value.trim();
+
+    const firstName = document.querySelector('#first-name').value.trim();
+    const lastName = document.querySelector('#last-name').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
-    if (name && email && password) {
-      const response = await fetch('/api/users', {
+    if (firstName && lastName && email && password) {
+      const response = await fetch('/api/users/', {
         method: 'POST',
-        body: JSON.stringify({ name, email, password }),
+        body: JSON.stringify({ firstName, lastName, email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
       if (response.ok) {
-        document.location.replace('/profile');
+        document.location.replace('/dashboard');
       } else {
         alert(response.statusText);
       }
